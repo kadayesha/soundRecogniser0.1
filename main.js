d94ab0a3b08c9c95b7cfaf9170fc580f9c7535ca
kadayesha <93032523+kadayesha@users.noreply.github.com>
2022-11-04 17:43:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//     
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({
+        audio:true
+    });
+    classifier=ml5.soundClassifier("",modelReady)
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults)
+}
+
+function gotResults(error,results)
+{
+    console.log("gotResult")
+}
